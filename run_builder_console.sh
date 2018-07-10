2fa3c6fa3f05272c96e96e7cbb2eff47a864b046
David B <37987466+dalijolijo@users.noreply.github.com>
2018-07-10 09:25:52 +0200

    Update run_builder_console.sh

--- run_builder_console.sh
+++ run_builder_console.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -it -h builder --name builder \
+docker run -rm -it -h builder --name builder \
 -v $THISDIR/cache:/shared/cache:Z \
 -v $THISDIR/result:/shared/result:Z \
 --entrypoint=/bin/bash \
