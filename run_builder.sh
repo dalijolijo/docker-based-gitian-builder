0175362fa1988e4e849538d668af9f5fd6cba8ab
David B <37987466+dalijolijo@users.noreply.github.com>
2018-07-10 09:25:34 +0200

    Update run_builder.sh

--- run_builder.sh
+++ run_builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -h builder --name builder \
+docker run --rm -h builder --name builder \
 -v $THISDIR/cache:/shared/cache:Z \
 -v $THISDIR/result:/shared/result:Z \
 builder
